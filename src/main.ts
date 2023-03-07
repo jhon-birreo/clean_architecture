ada5e8fec99166428ee9316995d4ac07e21f8605
jhonasbn <jbirreo.n96@gmail.com>
2023-03-07 00:26:23 -0500

    feat: ⚡️ new settings

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-
+import { App } from './core/infrastructure/app/index';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import 'reflect-metadata';
+async function main(): Promise<void> {
+	const app = new App();
+	app.listen();
+	await app.database();
+	// await app.redis();
+	app.routes();
+}
+void main();
